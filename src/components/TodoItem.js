97105bebcf1f09cb63e9983f1cab049ea3c73984
Rikstone <tarikbouari@gmail.com>
2022-09-14 12:11:41 +0000

    add Onclick event to the delete Btn

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,6 +10,7 @@ class TodoItem extends React.Component {
           onChange={() => this.props.changeHandlerProps(this.props.item.id)}
         />
         {this.props.item.title}
+        <button onClick={() =>this.props.deleteHandleProps(this.props.item.id)}> Delete</button>
       </li>
     );
   }
