248cb024633d99e00f1c9ef6f69f016307ccc6af
Rikstone <tarikbouari@gmail.com>
2022-09-15 11:26:53 +0000

    add style to the input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,53 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+ 
+  state = {
+    editing: false,
+  }
+  handleEdit = () => {
+    this.setState({
+      editing: true,
+    })
+  }
   render() {
+
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    let viewMode = {}
+    let editMode = {}
+
+    if(this.state.editing){
+      viewMode.display="none";
+    }else {
+      editMode.display ="none"
+    }
+    const {id,completed, title} = this.props.item
     return (
-      <li>
+      
+          <li className={styles.item}>
+            {/* <div onDoubleClick={this.handleEdit} style={viewMode} >
+
+            </div> */}
+            <input type="text" style={editMode} />
         <input
           type="checkbox"
-          checked={this.props.item.completed}
-          onChange={() => this.props.changeHandlerProps(this.props.item.id)}
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.changeHandlerProps(id)}
         />
-        {this.props.item.title}
-        <button onClick={() =>this.props.deleteHandleProps(this.props.item.id)}> Delete</button>
+        <span style={completed ? completedStyle : null}>
+        {title}
+        </span>
+        <button onClick={() =>this.props.deleteHandleProps(id)}> Delete</button>
       </li>
+      
     );
   }
 }
